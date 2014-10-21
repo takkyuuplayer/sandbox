3ebd9eb08f57adf41f06e98276fbef2d9e2b2d7d
takkyuuplayer <takkyuuplayer@gmail.com>
2014-10-21 16:39:35 +0000

    Add sending mail

--- symfony2/myproject/src/Blogger/BlogBundle/Controller/PageController.php
+++ symfony2/myproject/src/Blogger/BlogBundle/Controller/PageController.php
@@ -19,17 +19,28 @@ class PageController extends Controller
     {
         $enquiry = new Enquiry();
         $form = $this->createForm(new EnquiryType(), $enquiry);
-
         $request = $this->getRequest();
-        if($request->getMethod() === 'POST') {
-            $form->bind($request);
-            if($form->isValid()){
-                return $this->redirect($this->generateUrl('BloggerBlogBundle_contact'));
-            }
+
+        if($request->getMethod() === 'GET') {
+            return $this->render('BloggerBlogBundle:Page:contact.html.twig', array(
+                'form' => $form->createView()
+            ));
         }
 
-        return $this->render('BloggerBlogBundle:Page:contact.html.twig', array(
-            'form' => $form->createView()
-        ));
+        $form->bind($request);
+        if($form->isValid()){
+            $message = \Swift_Message::newInstance()
+                ->setSubject('Contact enquiry from symblog')
+                ->setFrom('enquiries@symblog.co.uk')
+                ->setTo($this->container->getParameter('blogger_blog.emails.contact_email'))
+                ->setBody($this->renderView('BloggerBlogBundle:Page:contactEmail.txt.twig', array('enquiry' => $enquiry)));
+            $this->get('mailer')->send($message);
+
+            $this->get('session')->getFlashBag()->add('blogger-notice', 'Your contact enquiry was successfully sent. Thank you!');
+
+            // Redirect - This is important to prevent users re-posting
+            // the form if they refresh the page
+            return $this->redirect($this->generateUrl('BloggerBlogBundle_contact'));
+        }
     }
 }
