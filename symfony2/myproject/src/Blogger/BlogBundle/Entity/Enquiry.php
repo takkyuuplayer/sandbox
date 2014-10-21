61d7c6fa0fcd9172e4b4cd5cdb4a4259cf00fc05
takkyuuplayer <takkyuuplayer@gmail.com>
2014-10-21 16:02:38 +0000

    Add validation

--- symfony2/myproject/src/Blogger/BlogBundle/Entity/Enquiry.php
+++ symfony2/myproject/src/Blogger/BlogBundle/Entity/Enquiry.php
@@ -2,6 +2,12 @@
 
 namespace Blogger\BlogBundle\Entity;
 
+use Symfony\Component\Validator\Mapping\ClassMetadata;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+
+
 class Enquiry
 {
     protected $name;
@@ -51,4 +57,21 @@ class Enquiry
     {
         $this->body = $body;
     }
+
+    public static function loadValidatorMetadata(ClassMetadata $metadata)
+    {
+        $metadata->addPropertyConstraint('name', new NotBlank());
+
+        $metadata->addPropertyConstraint('email', new Email());
+
+        $metadata->addPropertyConstraint('subject', new NotBlank());
+        $metadata->addPropertyConstraint('subject', new Length(array(
+            'max' => 50,
+        )));
+
+        $metadata->addPropertyConstraint('body', new Length(array(
+            'min' => 50,
+        )));
+    }
+
 }
