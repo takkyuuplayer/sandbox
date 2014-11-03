ad2720c0bd1fc380952b32860d954850d04d55e9
takkyuuplayer <takkyuuplayer@gmail.com>
2014-11-03 15:13:29 +0000

    Add comment view

--- symfony2/myproject/src/Blogger/BlogBundle/Entity/Repository/CommentRepository.php
+++ symfony2/myproject/src/Blogger/BlogBundle/Entity/Repository/CommentRepository.php
@@ -1,4 +1,5 @@
 <?php
+// src/Blogger/BlogBundle/Entity/Repository/CommentRepository.php
 
 namespace Blogger\BlogBundle\Entity\Repository;
 
@@ -12,4 +13,20 @@ use Doctrine\ORM\EntityRepository;
  */
 class CommentRepository extends EntityRepository
 {
+    public function getCommentsForBlog($blogId, $approved = true)
+    {
+        $qb = $this->createQueryBuilder('c')
+                   ->select('c')
+                   ->where('c.blog = :blog_id')
+                   ->addOrderBy('c.created')
+                   ->setParameter('blog_id', $blogId);
+
+        if (false === is_null($approved)) {
+            $qb->andWhere('c.approved = :approved')
+               ->setParameter('approved', $approved);
+        }
+
+        return $qb->getQuery()
+                  ->getResult();
+    }
 }
