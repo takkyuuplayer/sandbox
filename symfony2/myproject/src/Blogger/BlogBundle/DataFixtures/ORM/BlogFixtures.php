68d38997405a1a8522641392c058210ebc50aff7
takkyuuplayer <takkyuuplayer@gmail.com>
2014-11-03 15:00:08 +0000

    Add comment fixture data

--- symfony2/myproject/src/Blogger/BlogBundle/DataFixtures/ORM/BlogFixtures.php
+++ symfony2/myproject/src/Blogger/BlogBundle/DataFixtures/ORM/BlogFixtures.php
@@ -1,11 +1,12 @@
 <?php
 namespace Blogger\BlogBundle\DataFixtures\ORM;
 
+use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use Blogger\BlogBundle\Entity\Blog;
 
-class BlogFixtures implements FixtureInterface
+class BlogFixtures extends AbstractFixture implements FixtureInterface
 {
     public function load(ObjectManager $manager)
     {
@@ -60,5 +61,15 @@ class BlogFixtures implements FixtureInterface
         $manager->persist($blog5);
 
         $manager->flush();
+        $this->addReference('blog-1', $blog1);
+        $this->addReference('blog-2', $blog2);
+        $this->addReference('blog-3', $blog3);
+        $this->addReference('blog-4', $blog4);
+        $this->addReference('blog-5', $blog5);
+
+    }
+    public function getOrder()
+    {
+        return 1;
     }
 }
