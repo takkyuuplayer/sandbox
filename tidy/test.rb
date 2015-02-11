9b09828c40180874c26ba0a837850ef004d0493a
takkyuuplayer <takkyuuplayer@gmail.com>
2015-02-11 07:06:35 +0000

    Ugrify

--- tidy/test.rb
+++ tidy/test.rb
@@ -1,12 +1,1 @@
-require 'test/unit'
-
-class StackTest < Test::Unit::TestCase
-def test_PushAndPop
-stack = []; assert_same stack.length, 0
-stack.push 'foo'
-assert_equal stack.last, 'foo'
-assert_equal stack.length, 1
-assert_equal stack.pop, 'foo'
-assert_equal stack.length, 0
-end
-end
+require 'test/unit'; class StackTest < Test::Unit::TestCase; def test_PushAndPop; stack = []; assert_same stack.length, 0; stack.push 'foo'; assert_equal stack.last, 'foo'; assert_equal stack.length, 1; assert_equal stack.pop, 'foo'; assert_equal stack.length, 0; end; end;
