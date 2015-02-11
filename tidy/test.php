f81a68de67a9d8982c21394fee81eb42d30d3c5e
takkyuuplayer <takkyuuplayer@gmail.com>
2015-02-11 07:09:05 +0000

    Beautify

--- tidy/test.php
+++ tidy/test.php
@@ -1,1 +1,13 @@
-<?php require_once __DIR__.'/vendor/autoload.php'; class StackTest extends PHPUnit_Framework_TestCase { public function testPushAndPop() { $stack = array(); $this->assertEquals(0, count($stack)); array_push($stack, 'foo'); $this->assertEquals('foo', $stack[count($stack)-1]); $this->assertEquals(1, count($stack)); $this->assertEquals('foo', array_pop($stack)); $this->assertEquals(0, count($stack)); } }
+<?php require_once __DIR__.'/vendor/autoload.php'; class StackTest extends PHPUnit_Framework_TestCase
+ {
+     public function testPushAndPop()
+     {
+         $stack = array();
+         $this->assertEquals(0, count($stack));
+         array_push($stack, 'foo');
+         $this->assertEquals('foo', $stack[count($stack)-1]);
+         $this->assertEquals(1, count($stack));
+         $this->assertEquals('foo', array_pop($stack));
+         $this->assertEquals(0, count($stack));
+     }
+ }
