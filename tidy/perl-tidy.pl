9732c3abae6beee06f2bf3134e8378d0b6bda208
takkyuuplayer <takkyuuplayer@gmail.com>
2015-02-11 08:02:43 +0000

    Ugrify

--- tidy/perl-tidy.pl
+++ tidy/perl-tidy.pl
@@ -1,14 +1,1 @@
-use Test::More;
-use Test::Pretty;
-subtest 'Array' => sub {
-    subtest 'PushAndPop' => sub {
-        my @stack = ();
-        is scalar @stack, 0;
-        push @stack, 'foo';
-        is $stack[$#stack], 'foo';
-        my $last = pop @stack;
-        is $last, 'foo';
-        is scalar @stack, 0;
-    };
-};
-done_testing;
+use Test::More; use Test::Pretty; subtest 'Array' => sub { subtest 'PushAndPop' => sub { my @stack = (); is scalar @stack, 0; push @stack, 'foo'; is $stack[$#stack], 'foo'; my $last = pop @stack; is $last, 'foo'; is scalar @stack, 0; }; }; done_testing;
