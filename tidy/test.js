2fc2b150f144f89aa0656521783970539b82d4c2
takkyuuplayer <takkyuuplayer@gmail.com>
2015-02-11 07:21:42 +0000

    Beautify

--- tidy/test.js
+++ tidy/test.js
@@ -1,13 +1,13 @@
 var assert = require('power-assert');
 describe('Array', function() {
-    describe('PushAndPop()', function() {
-        it('should return index when the value is present', function() {
-            var stack = [];
-            assert.equal(stack.length, 0);
-            stack.push('foo');
-            assert.equal(stack[stack.length - 1], 'foo');
-            assert.equal(stack.pop(), 'foo');
-            assert.equal(stack.length, 0);
-        });
+  describe('PushAndPop()', function() {
+    it('should return index when the value is present', function() {
+      var stack = [];
+      assert.equal(stack.length, 0);
+      stack.push('foo');
+      assert.equal(stack[stack.length - 1], 'foo');
+      assert.equal(stack.pop(), 'foo');
+      assert.equal(stack.length, 0);
     });
+  });
 });
