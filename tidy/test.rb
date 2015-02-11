809597a149ea73f80a9fa38bc78c31cc9c598ca2
takkyuuplayer <takkyuuplayer@gmail.com>
2015-02-11 00:55:17 +0000

    Add test.rb

---
+++ tidy/test.rb
@@ -0,0 +1,12 @@
+require 'test/unit'
+
+class StackTest < Test::Unit::TestCase
+  def test_PushAndPop
+    stack = []; assert_same stack.length, 0
+    stack.push 'foo'
+    assert_equal stack.last, 'foo'
+    assert_equal stack.length, 1
+    assert_equal stack.pop, 'foo'
+    assert_equal stack.length, 0
+  end
+end
