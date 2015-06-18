c7f034cbdb5c807b4ac2b4005b5c701aa68a7d03
t-sekiguchi <takafumi_sekiguchi@voyagegroup.com>
2015-06-18 22:15:47 +0900

    Add dotfiles as local-git

--- cap3-test/config/deploy/vagrant.rb
+++ cap3-test/config/deploy/vagrant.rb
@@ -5,3 +5,9 @@ server 'localhost',
   ssh_options: {
     auth_methods: %w(publickey)
   }
+
+before :"rsync:sync", :import_local_git do
+  run_locally do
+    execute :git, :clone, 'git@github.com-tp:takkyuuplayer/dotfiles.git', "#{fetch(:rsync_src)}/local-git"
+  end
+end
