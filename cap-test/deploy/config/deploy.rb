8e6092369d8646774b93f1e387a9f6dd16d4aa66
t-sekiguchi <takafumi_sekiguchi@voyagegroup.com>
2015-06-18 11:08:24 +0900

    Add deploy script

--- cap-test/deploy/config/deploy.rb
+++ cap-test/deploy/config/deploy.rb
@@ -1,37 +1,16 @@
 set :application, "cap-test"
-set :repository,  "git@github.com:takkyuuplayer/cap-test.git"
+set :repository,  "git@github.com:takkyuuplayer/sandbox.git"
 
 role :localhost, "localhost"
 
 set :scm, :git
-set :branch, 'master'
+set :branch, 'dev-capistrano_with_local'
 
 set :use_sudo, false
 set :group_writtable, true
 
 set :deploy_via, :copy
-set :copy_cache,      '/workspace/cap-deployment/capcache'
-set :copy_dir,        '/workspace/cap-deployment/local-tmp'
-set :deploy_to,       '/workspace/cap-deployment/cap-test'
+set :copy_cache,      '/srv/work/cap-deployment/capcache'
+set :copy_dir,        '/srv/work/cap-deployment/local-tmp'
+set :deploy_to,       '/srv/work/cap-deployment/cap-test'
 set :normalize_asset_timestamps, false
-
-set :shared_children, %w(
-  web/cache
-)
-
-namespace :deploy do
-  task :setup_document_root do
-    run <<-CMD
-      cp -f #{current_path}/web/cache_test.php /workspace/sandbox/
-    CMD
-  end
-  task :clear_cache do
-    run <<-CMD
-      rm -rf #{shared_path}/realpath.cached &
-      rm -rf #{current_path}/web/cache/*
-    CMD
-  end
-end
-
-after 'deploy:finalize_update', 'deploy:setup_document_root'
-after 'deploy:restart', 'deploy:clear_cache'
