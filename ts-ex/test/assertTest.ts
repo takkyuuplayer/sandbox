41ba729c25aa20a02f0da533d8fcc681d073e56f
takkyuuplayer <takkyuuplayer@gmail.com>
2016-05-29 21:13:54 +0900

    Init test environment

---
+++ ts-ex/test/assertTest.ts
@@ -0,0 +1,11 @@
+/// <reference path="../typings/tsd.d.ts" />
+
+import * as assert from 'power-assert';
+
+describe("assert", () => {
+  it('has deepEqual', () => {
+    const a = {a:0, b:1};
+    const b = {a:0, b:1};
+  })
+});
+
